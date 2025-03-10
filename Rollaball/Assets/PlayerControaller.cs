75bf6ef689eccadbf9f4798a564567e354fafbad
moatassem22 <mohsenmoatassem@gmail.com>
2025-03-10 10:35:27 +0200

    project commit

---
+++ Rollaball/Assets/PlayerControaller.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.inputSystem;
+
+public class PlayerControaller : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    
+}
